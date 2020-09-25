28d9d6b583d5ccded890912c2a25bc009829f21f
lihon <eug.lihon@hotmail.com>
2020-09-25 18:26:17 +0200

    added base Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=140)
+    body = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article_detail_path', args=[str(self.id)])
