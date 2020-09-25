677b870049f49f66ae561127e7d83efd3e7910d4
lihon <eug.lihon@hotmail.com>
2020-09-25 18:28:09 +0200

    added base ArticleListView

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'articles_list'
+    template_name = 'article_list.html'
