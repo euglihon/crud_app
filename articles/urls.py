b8c47e46e74ee52546f5daaf3aeb1acda5c16a70
lihon <eug.lihon@hotmail.com>
2020-09-28 20:31:03 +0200

    added detail, update, delete views

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+
 
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list')
+    path('', ArticleListView.as_view(), name='article_list_path'),
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update_path'),
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail_path'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete_path'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,29 @@
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from .models import Article
+from django.urls import reverse_lazy
 
 
 class ArticleListView(ListView):
     model = Article
     context_object_name = 'articles_list'
     template_name = 'article_list.html'
+
+
+class ArticleDetailView(DeleteView):
+    model = Article
+    context_object_name = 'article'
+    template_name = 'article_detail.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    context_object_name = 'article'
+    fields = ['title', 'body']
+    template_name = 'article_edit.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'article'
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list_path')
